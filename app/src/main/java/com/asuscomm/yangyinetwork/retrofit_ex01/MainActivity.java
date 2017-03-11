7654e389465ed537cd8a235c80a71918f8fa3ff6
pjo901018 <pjo901018@korea.ac.kr>
2017-03-11 09:51:33 +0900

    delete 생성

--- app/src/main/java/com/asuscomm/yangyinetwork/retrofit_ex01/MainActivity.java
+++ app/src/main/java/com/asuscomm/yangyinetwork/retrofit_ex01/MainActivity.java
@@ -18,8 +18,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
-        get("1");
-        post("1","hello?");
+        //get("1");
+        post("1","sungsik is pool?");
+        //delete("1");
     }
 
     private void init(){
@@ -36,18 +37,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<Message> call, Response<Message> response) {
                 if (response.isSuccessful()) {
-                    Log.d("jyp", "success");
+                    Log.d("jyp", "get success");
                     mMessage = response.body();
                     Log.d("jyp", mMessage.getMessage());
                 } else {
-                    Log.d("jyp", "fail");
+                    Log.d("jyp", "get fail");
                     Log.d("jyp", response.errorBody().toString());
                 }
             }
 
             @Override
             public void onFailure(Call<Message> call, Throwable t) {
-                Log.d("jyp", "fail2");
+                Log.d("jyp", "get fail2");
                 t.printStackTrace();
             }
         });
@@ -61,22 +62,46 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<Message> call, Response<Message> response) {
                 if (response.isSuccessful()) {
-                    Log.d("jyp", "success");
+                    Log.d("jyp", "post success");
                     mMessage = response.body();
                     Log.d("jyp", mMessage.getMessage());
                 } else {
-                    Log.d("jyp", "fail");
+                    Log.d("jyp", "post fail");
                     Log.d("jyp", response.errorBody().toString());
                 }
             }
 
             @Override
             public void onFailure(Call<Message> call, Throwable t) {
-                Log.d("jyp", "fail2");
+                Log.d("jyp", "post fail2");
                 t.printStackTrace();
             }
         });
 
         Log.d("jyp","post");
+    }
+
+    private void delete(String id){
+        Call<Void> call = mMessageService.deleteRecord(id);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    Log.d("jyp", "delete success");
+                    Log.d("jyp", response.toString());
+                } else {
+                    Log.d("jyp", "delete fail");
+                    Log.d("jyp", response.errorBody().toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Log.d("jyp", "delete fail2");
+                t.printStackTrace();
+            }
+        });
+
+        Log.d("jyp","delete");
     }
 }
